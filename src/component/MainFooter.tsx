4c8d08c7850cff418248f5f2575175a96c0bdc44
Sabri KHOUALDIA <sk@self-med.fr>
2021-06-06 13:29:30 +0200

    fix

--- src/component/MainFooter.tsx
+++ src/component/MainFooter.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Divider, Grid, makeStyles, Typography} from "@material-ui/core";
+import {Divider, Grid, IconButton, makeStyles, Typography} from "@material-ui/core";
 import PhoneIcon from '@material-ui/icons/Phone';
 import HomeIcon from '@material-ui/icons/Home';
 import instasvg from "./../static/instagram.svg";
@@ -39,6 +39,14 @@ const useStyles = makeStyles((theme) => ({
 export const MainFooter: React.FC = () => {
   const classes = useStyles();
 
+  function handleClickFb() {
+    window.open('https://www.facebook.com/Laboussole74', '_blank');
+  }
+
+  function handleClickInsta() {
+    window.open('https://www.instagram.com/laboussole74/', '_blank');
+  }
+
   return (
     <div className={classes.root}>
       <Grid spacing={2} container className={classes.main}>
@@ -61,7 +69,7 @@ export const MainFooter: React.FC = () => {
         </Grid>
       </Grid>
 
-      <Divider className={classes.divider} />
+      <Divider className={classes.divider}/>
 
       <Grid container spacing={2} className={classes.second}>
         <Grid item xs={3}/>
@@ -72,16 +80,22 @@ export const MainFooter: React.FC = () => {
         </Grid>
         <Grid container item xs={6}>
           <Grid item xs={2}>
-            <Typography color={"secondary"} className={classes.iconTextVertically}>
-              <img src={snapsvg} style={{height: 20}} alt={"logo"}/>
-              laboussole74
-            </Typography>
+            <IconButton aria-label="snap">
+              <Typography color={"secondary"} className={classes.iconTextVertically}>
+                <img src={snapsvg} style={{height: 20}} alt={"logo"}/>
+                laboussole74
+              </Typography>
+            </IconButton>
           </Grid>
           <Grid item xs={2}>
-            <img src={fbsvg} style={{height: 20}} alt={"logo"}/>
+            <IconButton aria-label="facebook" onClick={handleClickFb}>
+              <img src={fbsvg} style={{height: 20}} alt={"logo"}/>
+            </IconButton>
           </Grid>
           <Grid item xs={2}>
-            <img src={instasvg} style={{height: 20}} alt={"logo"}/>
+            <IconButton aria-label="insta" onClick={handleClickInsta}>
+              <img src={instasvg} style={{height: 20}} alt={"logo"}/>
+            </IconButton>
           </Grid>
         </Grid>
         <Grid item xs={1}/>
